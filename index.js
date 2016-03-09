07f5e0914cf64c4455dca5bec5b429e6956efc9f
Sander Ploegsma <sanderploegsma@gmail.com>
2016-03-09 23:39:52 +0100

    Use slash command instead of bot
    Create LICENSE

--- index.js
+++ index.js
@@ -1,74 +1,41 @@
-var Slack     = require('slack-api').promisify();
-var Botkit    = require('botkit');
 var github    = require('octonode');
+var express   = require('express');
+var app       = express();
 
-var botName = process.env.BOT_NAME;
-var slackToken = process.env.BOT_TOKEN;
+var bodyParser = require('body-parser')
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
+
+var slackToken = process.env.SLACK_TOKEN;
 var githubToken = process.env.GITHUB_TOKEN;
 var githubRepository = process.env.GITHUB_REPO;
 
-var controller = Botkit.slackbot();
-var bot = controller.spawn({
-  token: slackToken
-});
+var port = process.env.PORT || 80;
 
-bot.startRTM(function(err, bot, payload) {
-  if (err) {
-    throw new Error("Error connecting to slack!");
-  }
-});
-
-controller.hears(["keyword","^.+$"], ["direct_mention", "mention"], function(bot, message) {
-  console.log("Bot was mentioned!");
-
-  // Get the info from the user who mentioned
-  Slack.users.info({
-    "token": slackToken,
-    "user": message.user
-  }).then(function(data) {
-    if (data.ok != true) {
-      console.log(err);
-      bot.reply(message, "Who the hell are you?!");
-      return;
-    }
-    user = data.user.name;
-    return user;
-  }).then(function(user) {
-    // Convert additional mentions to usernames
-    // This sucks because it is sent asynchronously and returns too late. Should fix
-
-    // var regex = new RegExp('<@([A-Z0-9]+)>', 'g');
-    // var text = message.text.replace(regex, function(m,c) {
-    //   Slack.users.info({
-    //     "token": slackToken,
-    //     "user": c
-    //   }, function(err,data) {
-    //     if (err || data.ok != true) {
-    //       return "unknown user";
-    //     }
-    //     return (data.user.name !== botName) ? data.user.name : "";
-    //   });
-    // });
-
-    var regex = new RegExp('<@([A-Z0-9]+)>', 'g');
-    var text = message.text.replace(regex, "");
-
-    // Create Github issue!
+app.post('/', function(req, res) {
+  console.log(req);
+  if (req.body.token !== slackToken) {
+    return res.status(403).end();
+  } else {
     var client = github.client(githubToken);
-
     client.repo(githubRepository).issue({
-      title: text,
-      body: "Toegevoegd door " + user,
+      title: req.body.text,
+      body: "Added by " + req.body.user_name,
       labels: ['slack']
     }, function(err, data, headers) {
       if (err) {
         console.log(err, data);
-        bot.reply(message, "Er is iets misgegaan bij Github, wat stom.");
+        return res.status(500).end();
       } else {
-        bot.reply(message, "Oehoe! Goed idee, " + user + ", we gaan het voor je fixen!");
+        return res.status(200).json({
+          text: "Oehoe! Goed idee, " + req.body.user_name + ", we gaan het voor je fixen!"
+        });
       }
     });
-  }).catch(function(e) {
-    console.log("An error occured: " + e);
-  });
+  }
+});
+
+app.listen(port, function () {
+  console.log('Slack bot listening on port ' + port);
 });
