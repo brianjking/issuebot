1c10f435d133ea466de319eddc819de91872d216
Sander Ploegsma <sanderploegsma@gmail.com>
2016-03-09 22:49:48 +0100

    Basic script

--- index.js
+++ index.js
@@ -1,13 +1,78 @@
-var SlackBot = require('slackbots');
+// var settings  = require('./settings.json');
+var Slack     = require('slack-api').promisify();
+var Botkit    = require('botkit');
+var github    = require('octonode');
 
 var botName = process.env.BOT_NAME;
 var slackToken = process.env.BOT_TOKEN;
+var githubToken = process.env.GITHUB_TOKEN;
+var githubRepository = process.env.GITHUB_REPO;
 
-var bot = new SlackBot({
-  token : slackToken,
-  name  : botName
+// var botName = settings.botName;
+// var slackToken = settings.slackToken;
+
+var controller = Botkit.slackbot();
+var bot = controller.spawn({
+  token: slackToken
 });
 
-bot.on('message', function(data) {
-  console.log(data);
-})
+bot.startRTM(function(err, bot, payload) {
+  if (err) {
+    throw new Error("Error connecting to slack!");
+  }
+});
+
+controller.hears(["keyword","^.+$"], ["direct_mention", "mention"], function(bot, message) {
+  console.log("Bot was mentioned!");
+
+  // Get the info from the user who mentioned
+  Slack.users.info({
+    "token": slackToken,
+    "user": message.user
+  }).then(function(data) {
+    if (data.ok != true) {
+      console.log(err);
+      bot.reply(message, "Who the hell are you?!");
+      return;
+    }
+    user = data.user.name;
+    return user;
+  }).then(function(user) {
+    // Convert additional mentions to usernames
+    // This sucks because it is sent asynchronously and returns too late. Should fix
+
+    // var regex = new RegExp('<@([A-Z0-9]+)>', 'g');
+    // var text = message.text.replace(regex, function(m,c) {
+    //   Slack.users.info({
+    //     "token": slackToken,
+    //     "user": c
+    //   }, function(err,data) {
+    //     if (err || data.ok != true) {
+    //       return "unknown user";
+    //     }
+    //     return (data.user.name !== botName) ? data.user.name : "";
+    //   });
+    // });
+
+    var regex = new RegExp('<@([A-Z0-9]+)>', 'g');
+    var text = message.text.replace(regex, "");
+
+    // Create Github issue!
+    var client = github.client(githubToken);
+
+    client.repo(githubRepository).issue({
+      title: text,
+      body: "Toegevoegd door " + user,
+      labels: ['slack']
+    }, function(err, data, headers) {
+      if (err) {
+        console.log(err, data);
+        bot.reply(message, "Er is iets misgegaan bij Github, wat stom.");
+      } else {
+        bot.reply(message, "Oehoe! Goed idee, " + user + ", we gaan het voor je fixen!");
+      }
+    });
+  }).catch(function(e) {
+    console.log("An error occured: " + e);
+  });
+});
